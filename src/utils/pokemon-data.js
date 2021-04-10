b91595055917c80ba1b8499b81e1aabf9f2f5d60
framirez <frank.ramirez@endurance.com>
2021-04-09 21:16:20 -0400

    Initial server files added, endpoints created

---
+++ src/utils/pokemon-data.js
@@ -0,0 +1,23 @@
+const axios = require('axios');
+
+const pokemonData = (pokemonName, callback) => {
+    const url = `https://pokeapi.co/api/v2/pokemon/${pokemonName.toLowerCase()}`;
+    axios.get(url)
+    .then(res => {
+        const mainData = res.data;
+        const species = mainData.species.url;
+
+        axios.get(species).then(res => {
+            const speciesData = res.data;
+
+            const evolution = speciesData.evolution_chain.url;
+            axios.get(evolution).then(res => {
+                callback(undefined, mainData, speciesData, res.data);
+            });
+        });
+    }).catch(err => {
+        callback('No pokemon found under this name. Please try another one.', undefined);
+    });
+}
+
+module.exports = pokemonData;
